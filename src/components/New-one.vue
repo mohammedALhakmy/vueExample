b1e297844892c05736aa8196bc8f181f0e442925
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-03-27 03:45:15 +0300

    second commit

--- src/components/New-one.vue
+++ src/components/New-one.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>Hello Developer {{ email }} - {{ mobile }} - {{ getName() }}</h1>
+    <span>{{ getSecond("Ali") }}</span>
+    <h3>{{ getData().email }}</h3> <br>
+    <h3>{{ getData().phone }}</h3>
 </template>
 
 <script>
@@ -11,13 +14,22 @@ export default {
             mobile:"0502879159",
             getName: function(){
                 return "Moahmmed";  
+            },
+            getSecond:function(nameE){
+                return nameE
+            },
+            getData(){
+                return {
+                    email:"test1@test.com",
+                    phone:"0502879159",
+                }
             }
         }
     }
 }
 </script>
 <style>
-h1{
-    color:red;
-}
+    h1 {
+        color: red;
+    }
 </style>
