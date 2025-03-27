27f5032dffc919be554dd9d9f56cd997ee480763
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-03-27 03:24:01 +0300

    start first commit

---
+++ components/New-one.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello Developer {{ email }} - {{ mobile }} - {{ getName() }}</h1>
+</template>
+
+<script>
+export default {
+    name:"Pages",
+    data(){
+        return{
+            email:"test@cod6e.com",
+            mobile:"0502879159",
+            getName: function(){
+                return "Moahmmed";  
+            }
+        }
+    }
+}
+</script>
+<style>
+h1{
+    color:red;
+}
+</style>
